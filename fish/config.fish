04794a883ded9d2fe48536368d572f0cceb86dde
SakibShahariar <sakib209870@gmail.com>
2025-02-07 16:58:13 +0600

    commit

--- fish/config.fish
+++ fish/config.fish
@@ -12,7 +12,10 @@ set -e PATH
 
 # Set the correct PATH including ~/Scripts first, followed by system directories
 set -Ux PATH $PATH ~/Scripts /usr/bin /usr/local/bin /home/sakib/.local/bin /home/sakib/.cargo/bin /usr/local/sbin ~/sbin
+set -Ux QT_QPA_PLATFORMTHEME "qt6ct:gtk2"
 set -Ux PATH /sbin /usr/sbin $PATH
+set -Ux QT_STYLE_OVERRIDE Kvantum
+
 
 # Starship Initialization (Prompt)
 starship init fish | source
