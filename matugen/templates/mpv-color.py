8e2e21a21f0c212e865d119f3752d42791abf29b
Sakib <sakib209870@gmail.com>
2025-03-07 09:03:29 +0600

    commit

--- matugen/templates/mpv-color.py
+++ matugen/templates/mpv-color.py
@@ -1,1 +1,1 @@
-color=foreground={{colors.primary.default.hex}},foreground_text=282828,background=282828,background_text={{colors.primary.default.hex}},curtain=1d2021,success=b8bb26,error=fb4934
+color=foreground={{colors.primary.default.hex}},foreground_text=000000aa,background=000000aa,background_text={{colors.primary.default.hex}},curtain=1d2021,success=b8bb26,error=fb4934
--- mpv/color.py
+++ mpv/color.py
@@ -1,1 +1,1 @@
-color=foreground=#f8b1dd,foreground_text=282828,background=282828,background_text=#f8b1dd,curtain=1d2021,success=b8bb26,error=fb4934
+color=foreground=#ffb786,foreground_text=000000aa,background=000000aa,background_text=#ffb786,curtain=1d2021,success=b8bb26,error=fb4934
