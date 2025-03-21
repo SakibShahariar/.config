8dd8056ecca52cb289c73d1ab8b001a168a7f49f
Sakib <sakib209870@gmail.com>
2025-03-22 00:07:31 +0600

    commit

--- mpv/color.py
+++ mpv/color.py
@@ -1,1 +1,1 @@
-color=foreground=#bac3ff,foreground_text=000000aa,background=000000aa,background_text=#bac3ff,curtain=1d2021,success=b8bb26,error=fb4934
+color=foreground=#ffb599,foreground_text=000000aa,background=000000aa,background_text=#ffb599,curtain=1d2021,success=b8bb26,error=fb4934
